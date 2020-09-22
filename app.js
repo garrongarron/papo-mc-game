1d416de44a5cda0bf9e4e354b67a85d0c1f405c8
Federico Zacayan <federico.zacayan@gmail.com>
2020-09-22 09:46:25 -0300

    005-Esperar 5+10 segundos

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 let interval = null
-let time = 5
+let value = location.search.replace('?','')
+let time = (value=='')?5:10
+
 let setTime = (time) =>{
     document.querySelector('strong').innerText = time
     if(time==0){
         clearInterval(interval)
         let link = document.querySelector('a')
-        link.setAttribute('href','ganar.html')
+        if(value==2){
+            link.setAttribute('href','ganar.html')
+        } else{
+            link.setAttribute('href','siguiente.html')
+        }
     }
 }
 setTime(time)
